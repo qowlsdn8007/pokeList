77bb5a0fc9d5e54bdc87f21c8449fa8537ca55c3
jinwoo <qowlsdn8007@gmail.com>
2023-04-16 15:56:49 +0900

    chore: styled-component 적용, global-style 적용

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
-import "./App.css";
+import GlobalStyle from './GlobalStyle.tsx';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <a
-          className="App-link"
-          href="https://socarcorp.kr"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Happy Coding 🤗
-        </a>
-      </header>
+      <GlobalStyle />
     </div>
   );
 }
---
+++ src/GlobalStyle.tsx
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  *, *::before, *::after {
+    box-sizing: border-box;
+  }
+  html {
+    background-color: #f2f4f6;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 100%;
+  }
+
+  body {
+    line-height: 1;
+    max-width: 420px;
+    height: 100vh;
+    background-color: #fff;
+    margin: 0;
+    padding: 0;
+  }
+`;
+
+export default GlobalStyle;
